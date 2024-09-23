8bb939326f658f388d6433b43013639002c60e65
Benoit81 <ben81@ldebian.localhost>
2024-09-23 22:07:51 +0200

    update script git

--- generateDoc.sh
+++ generateDoc.sh
@@ -1,7 +1,7 @@
 pandoc --pdf-engine=pdfroff --toc-depth=1 README.MD -o dist/README.pdf
 for  d in $(find doc -name "*.md")
 do
-    pandoc --pdf-engine=pdfroff --toc-depth=1 $d -o dist/$(basename $d .md).pdf
+    pandoc --pdf-engine=pdfroff --toc-depth=1 $d -o dist/$(basename $d .md).pdf --verbose
 done
 
 
--- gitUtil.sh
+++ gitUtil.sh
@@ -81,12 +81,36 @@ function needCommit() {
 }
 
 function initAliasCdGitRepo() {
+    local OLD=$PWD
     for d  in $( repoGit )
     do
         folder=$(dirname $d)
         name=$(basename $folder)
-        echo "alias cd${name}=\"cd $folder\""
         alias cd${name}="cd $folder"
+        alias cd${name}
+        # create alias from pom.xml project
+        for pom in $(git --git-dir=$d ls-files -- '**/pom.xml')
+        do
+            #  echo ${folder}/${pom}
+            pomfolder=$(dirname  ${folder}/${pom})
+            artifact=$(xmllint --shell ${folder}/${pom} <<< "setns ns=http://maven.apache.org/POM/4.0.0
+            cat /ns:project/ns:artifactId/text()" | grep -v "^/" )
+            if  [[ $? -eq 0 ]]
+            then
+                alias cd${artifact}="cd $pomfolder"
+                alias cd${artifact}
+            fi
+        done
+        # create alas from package.json project
+        for json in $(git --git-dir=$d ls-files -- '**/package.json')
+        do
+            # echo ${folder}/${json}
+            jsonfolder=$(dirname  ${folder}/${json})
+            jsonName=$( jq -r .name ${folder}/${json})
+            alias cd${jsonName}="cd $jsonfolder"
+            alias cd${jsonName}
+        done
     done
-
+    echo "return to $OLD"
+    cd "$OLD"
 }
