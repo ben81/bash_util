a60ab2f49b7b6b8317a92eb8295bbf3fa0c23127
Benoit81 <ben81@ldebian.localhost>
2025-05-04 19:05:50 +0200

    update prompt

--- prompt.sh
+++ prompt.sh
@@ -4,4 +4,5 @@
 
 
 PROMPT_COMMAND='PS1_CMD1=$(currentBranchGit)$(tracking_info)$(needCommit)';
-PS1='\[\e[38;5;171m\]\u@\h\[\e[0m\]:\[\e[38;5;83m\]\w\[\e[0m\] \[\e[91m\]${PS1_CMD1}\[\e[0m\] \\$ '
+#PS1='\[\e[38;5;171m\]\u@\h\[\e[0m\]:\[\e[38;5;83m\]\w\[\e[0m\] \[\e[91m\]${PS1_CMD1}\[\e[0m\] \\$ '
+PS1='\[\e[38;5;171m\]\u@\h\[\e[0m\]:\[\e[38;5;83m\]\w\[\e[0m\] \[\e[30;101m\]${PS1_CMD1}\[\e[0;91m\]\[\e[0m\]\\$ '
