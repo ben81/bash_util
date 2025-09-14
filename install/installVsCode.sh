9b259d8f4276c0b019006fd1ed71cea7c562b114
Benoit81 <ben81@ldebian.localhost>
2025-09-14 19:53:24 +0200

    install vscode

---
+++ install/installVsCode.sh
@@ -0,0 +1,15 @@
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
+rm -f microsoft.gpg
+sudo echo "Types: deb
+URIs: https://packages.microsoft.com/repos/code
+Suites: stable
+Components: main
+Architectures: amd64,arm64,armhf
+Signed-By: /usr/share/keyrings/microsoft.gpg
+"> /etc/apt/sources.list.d/vscode.source
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
