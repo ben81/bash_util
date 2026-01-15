355a8ef69544933bb29c8bb69c9fbd48fe1d8931
Ben81 <ben81@ldebian.localhost>
2026-01-15 08:50:38 +0100

    update generation

--- util/generateDoc.sh
+++ util/generateDoc.sh
@@ -1,7 +1,8 @@
-pandoc --pdf-engine=pdfroff --toc-depth=3  -N  README.MD -o dist/README.pdf
+mkdir -p dist
+pandoc --pdf-engine=pdflatex --toc-depth=3  -N  README.MD -o dist/README.pdf
 for  d in $(find doc -name "*.md")
 do
-    pandoc --pdf-engine=pdfroff --toc-depth=3  -N  $d -o dist/$(basename $d .md).pdf --verbose
+    pandoc --pdf-engine=pdflatex --toc-depth=3  -N  $d -o dist/$(basename $d .md).pdf --verbose
 done
 
 
