2bf6348166549bcd44bfc43a9996edd7ad96797f
Benoit81 <ben81@ldebian.localhost>
2026-08-29 15:05:27 +0200

    optimize script

--- git/gitListAllBranch.sh
+++ git/gitListAllBranch.sh
@@ -9,4 +9,11 @@
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-find $PWD -type d -name ".git" | awk '{ print "cd $( dirname "$0") && echo \"================================ $PWD\n\"  && git branch -avv --no-abbrev && echo" }' | sh
+for d in $(find $PWD -type d -name ".git" )
+do 
+	cd $( dirname "$d")
+	echo "================================ $PWD"
+	git --no-pager branch -avv --no-abbre 
+	echo
+done
+
